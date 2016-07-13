58da12171af82f7dd07246795037c98203558c1a
korzecmaku <korzecmaku@o2.pl>
2016-07-13 20:47:05 +0200

    Forms view wireframe

--- app/frontend/app.js
+++ app/frontend/app.js
@@ -3,6 +3,8 @@
 // Declare app level module which depends on views, and components
 angular.module('thesisApp', [
   'ngRoute',
+  // 'jquery',
+  // 'ui.bootstrap',
   'thesisApp.form'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
--- app/frontend/form/form.js
+++ app/frontend/form/form.js
@@ -9,6 +9,42 @@ angular.module('thesisApp.form', ['ngRoute'])
   });
 }])
 
-.controller('FormCtrl', ['$scope', function($scope) {
-  $scope.helloWorld = "Hello World"
+.controller('FormCtrl', ['$scope', '$modalInstance', function($scope, $modalInstance) {
+  $scope.info = {};
+
+  $scope.gender = ["Kobieta", "Mężczyzna"];
+
+  $scope.eyes = {
+    myopia: {
+      text: "krótkowzroczność",
+      left: false,
+      right: false
+    },
+    presbyopia: {
+      text: "dalekowzroczność",
+      left: false,
+      right: false
+    },
+    astigmatism: {
+      text: "astygmatyzm",
+      left: false,
+      right: false
+    },
+    dichromatism: {
+      text: "dichromatyzm",
+      right: false
+    },
+    trichromatism: {
+      text: "trichromatyzm",
+      right: false
+    },
+    monochromatism: {
+      text: "monochromatyzm",
+      right: false
+    }
+  }
+
+  $scope.openModal = new function () {
+    $modalInstance.open();
+  }
 }]);
